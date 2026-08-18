a15423b68b7f0736af0579059a872e45aca82d77
Kyno <40477759+ImKyno@users.noreply.github.com>
2026-08-18 17:46:14 -0300

    Returfed! - Fixed Pigeon Spawn

--- modinfo.lua
+++ modinfo.lua
@@ -1,11 +1,11 @@
 name                           = "Not Enough Turfs"
-version                        = "1.3-B"
+version                        = "1.4-B"
 
 description                    =
 [[
 󰀪 Adds over 100 brand new Turfs to craft and use in your world!
 
-󰀌 Mod Version: 1.3-B
+󰀌 Mod Version: 1.4-B
 󰀧 Update: Returfed!
 ]]
 
